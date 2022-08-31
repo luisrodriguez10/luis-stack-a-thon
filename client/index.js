2662044b435cd2c9164de914b5620e2f8f29b3e6
Luis Rodriguez <luisu_10@hotmail.com>
2022-08-31 00:25:26 -0500

    Updated index component to use createRoot

--- client/index.js
+++ client/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'; 
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './App'
 
-ReactDOM.render(
+const app = createRoot(document.querySelector('#root'));
+
+app.render(
   <Provider store={store}>
     <Router history={history}>
       <App />
     </Router>
-  </Provider>,
-  document.getElementById('app')
+  </Provider>
 )
