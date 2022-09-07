6ab3a4b7962a681d0d2e30609b4ea58c1fdf28d7
Luis Rodriguez <luisu_10@hotmail.com>
2022-09-07 15:58:34 -0500

    Updated library for react icons

--- client/components/PastTrips.js
+++ client/components/PastTrips.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { TbFilesOff } from "react-icons/Tb";
+import { TbFilesOff } from "react-icons/tb";
 import {
   fetchBuses,
   fetchStatuses,
--- client/components/Tracking.js
+++ client/components/Tracking.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { TbFilesOff } from "react-icons/Tb";
+import { TbFilesOff } from "react-icons/tb";
 import Table from "react-bootstrap/Table";
 import MapParent from "./MapParent";
 import {
